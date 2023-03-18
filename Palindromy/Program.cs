782b6f6d44d9a488527f17db056a61f054c1aa8c
Tomek <Tomek@LAPTOP-LN86A4AV>
2023-03-18 09:57:33 +0100

    Add project files.

---
+++ Palindromy/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security;
+
+namespace Lets_Code_Somethings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word;
+            bool isPalindrom = true;
+  
+            word = Console.ReadLine().Replace(" ", string.Empty).ToLower();  
+            for(int i =0; i < word.Length/2; i++)
+            {
+                if (word[i] != word[word.Length - 1 - i])
+                {
+                    isPalindrom= false;
+                    break;
+                }
+            }
+            if(isPalindrom)
+            {
+                Console.WriteLine("Tak");
+            }
+            else
+            {
+                Console.WriteLine("Nie");
+            }
+        }
+    }
+}
